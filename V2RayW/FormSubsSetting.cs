6fa15c3fb0f7e4cfddaf88a3e83a42a3bbce9cc0
ZK <me@ll.com>
2020-06-11 14:29:32 +0800

    commit all

---
+++ V2RayW/FormSubsSetting.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace V2RayW
+{
+    public partial class FormSubsSetting : Form
+    {
+        //public static List<SubProfile> subs = new List<SubProfile>();
+
+        public FormSubsSetting()
+        {
+            InitializeComponent();
+            I18N.InitControl(this);
+        }
+
+        private void FormSubsSetting_Load(object sender, EventArgs e)
+        {
+            foreach (SubsProfile item in Program.subsProfiles)
+            {
+                listBoxSubs.Items.Add(item.remark + "-" + item.url);
+            }
+        }
+
+        private void buttonAddSub_Click(object sender, EventArgs e)
+        {
+            string url = textBoxSubUrl.Text;
+            if (url.Length > 0)
+            {
+                string remark = textBoxRemark.Text;
+                if (remark.Length == 0)
+                {
+                    remark = "new" + (Program.subsProfiles.Count);
+                }
+                SubsProfile sub = new SubsProfile(url, remark);
+                sub.groupId = Program.subsProfiles.Count();
+                while (Program.groupSubsMap.ContainsKey(sub.groupId))
+                    sub.groupId++;
+                Program.subsProfiles.Add(sub);
+                Program.subsProfiles[0].SubsRefList.Add(sub);
+                Program.groupSubsMap.Add(sub.groupId, sub);
+                DataRow dr = Program.dt.NewRow();
+                dr["remark"] = remark;
+                dr["url"] = url;
+                //dr["groupId"] = sub.groupId;
+                Program.dt.Rows.Add(dr);
+                Program.bs.ResetBindings(false);
+                listBoxSubs.Items.Add(remark + "-" + url);
+            }
+        }
+
+        //-
+        private void textBoxSubUrl_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            var selectedIndex = listBoxSubs.SelectedIndex;
+            if (selectedIndex > 0 && selectedIndex < Program.subsProfiles.Count)
+            {
+                listBoxSubs.Items.RemoveAt(selectedIndex);
+                SubsProfile deletedSubs = Program.subsProfiles[selectedIndex];
+                Program.subsProfiles.Remove(deletedSubs);
+                Program.subsProfiles[0].SubsRefList.Remove(deletedSubs);
+                deletedSubs.VmessConfList.Clear();
+                if (Program.groupSubsMap.ContainsKey(deletedSubs.groupId))
+                    Program.groupSubsMap.Remove(deletedSubs.groupId);
+
+                Program.dt.Rows.RemoveAt(selectedIndex);
+                Program.bs.ResetBindings(false);
+            }
+        }
+    }
+}
---
+++ V2RayW/IOSerialize.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace V2RayW
+{
+    class IOSerialize
+    {
+        public static JavaScriptSerializer js = new JavaScriptSerializer();
+
+        public static void writingJson(SubsProfile subsProfile, string dir = "")
+        {
+            string filename = Utils.MD5Sum(subsProfile.remark + "-" + subsProfile.url);
+            string json = js.Serialize(subsProfile);
+            save(string.Format("{0}\\{1}.json", dir, filename), json);
+        }
+
+        private static void save(string path, string jsonStr)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(jsonStr);
+                }
+            }
+        }
+
+        private static string getJsonStr(string path)
+        {
+            using (FileStream fsRead = new FileStream(path, FileMode.Open))
+            {
+                int fsLen = (int)fsRead.Length;
+                byte[] heByte = new byte[fsLen];
+                int r = fsRead.Read(heByte, 0, heByte.Length);
+                return System.Text.Encoding.UTF8.GetString(heByte);
+            }
+        }
+
+        public static SubsProfile readingJson(string path)
+        {
+            string jsonStr = getJsonStr(path);
+            SubsProfile temp = js.Deserialize<SubsProfile>(jsonStr);
+            return temp;
+        }
+    }
+}
